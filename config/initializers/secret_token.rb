f44b573ec3aac3d84e8a886bcf048977dc07f5e8
André Hester <ahester@mb-ahester.local>
2012-11-30 11:29:19 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linklist::Application.config.secret_token = '2e33b5a456afbfbb8d7b28848beaf8c5edac6fad9278fa2889beab9c319ea77f26e6928ffd2eff63668d6bc9edfa204d9c2766df5804ce41cbb99dc1291dfbaf'
